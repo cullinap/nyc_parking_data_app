9ed8f5f9bab88a99b774ee951f9becc7bddd7411
Patrick Cullinane <cullinap@gmail.com>
2023-11-07 22:31:04 -0500

    test index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-const userApiRoutes = require("./UserApi");
+//const userApiRoutes = require("./UserApi");
 
-const constructorMethod = (app) => {
-  app.use("/", userApiRoutes);
+// const constructorMethod = (app) => {
+//   app.use("/", userApiRoutes);
 
-  app.use("*", (req, res) => {
-    res.status(404).json({ error: "Not Found" });
-  });
-};
+//   app.use("*", (req, res) => {
+//     res.status(404).json({ error: "Not Found" });
+//   });
+// };
 
 module.exports = constructorMethod;
